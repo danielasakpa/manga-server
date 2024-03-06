6aca05625e013596315dab6722dc5142ac8c2092
danielasakpa <danielasakpa@gmail.com>
2024-03-06 22:37:48 +0100

    feat: Enhance authentication routes
    - Added Google authentication routes with '/google' and '/google/callback'.
    - Introduced 'googleAuth' middleware for initiating Google authentication.
    - Added 'googleAuthCallback' route for handling Google authentication callback.
    - Expanded the existing 'login' route for local authentication.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { login } = require('../controllers/authController');
+const { login, googleAuthCallback } = require('../controllers/authController');
+const passport = require('passport');
+
+
+const googleAuth = passport.authenticate('google', { scope: ['profile', 'email'] });
 
 // Login
 router.post('/login', login);
+
+router.get('/google', googleAuth);
+
+router.get('/google/callback', googleAuthCallback);
 
 module.exports = router;
