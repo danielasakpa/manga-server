e19b79d690d8ef354cf88f5641f88f3770cb3cd0
danielasakpa <danielasakpa@gmail.com>
2024-03-08 01:51:17 +0100

    feat: Implement Google OAuth2.0 authentication strategy in Passport.js
    Configure Passport.js with a Google OAuth2.0 strategy to authenticate users. This includes handling the user's information, such as email and display name, and saving it to the database. The strategy uses the Google client ID, client secret, and a callback URL for authentication.
    
    Also, implement serialization and deserialization functions in Passport.js to manage user sessions.

---
+++ utils/passport.js
@@ -0,0 +1,47 @@
+const User = require('../models/user');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// Configure Passport.js with Google OAuth2.0
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: 'https://manga-server-luxr.onrender.com/api/auth/google/callback'
+},
+    async function (accessToken, refreshToken, profile, done) {
+        try {
+            // Here you can save the user's information to your database
+            const userEmail = profile.emails[0].value;
+
+            // Find the user by email
+            let user = await User.findOne({ email: userEmail });
+
+            if (user) {
+                // User already exists, just return the user
+                return done(null, user);
+            } else {
+                // Create a new user
+                const newUser = new User({
+                    username: profile.displayName,
+                    email: userEmail,
+                    password: profile.id
+                });
+
+                // Save the new user and return it
+                user = await newUser.save();
+                return done(null, user);
+            }
+        } catch (err) {
+            return done(err);
+        }
+    }
+));
+
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
