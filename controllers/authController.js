a12a8b256bb6bdd3af6b7dc77217c87163f11de2
danielasakpa <danielasakpa@gmail.com>
2024-03-13 08:14:15 +0100

    Updatecookie configurations

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,6 +19,7 @@ const passportLogin = (req, res) => {
         const cookieOptions = {
             secure: true,
             sameSite: 'none',
+            httpOnly: true,
             maxAge: 24 * 60 * 60 * 1000
         };
 
