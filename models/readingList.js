74304c64bd9f16c4273c803c69ada5e1577a7763
danielasakpa <danielasakpa@gmail.com>
2024-03-15 18:52:27 +0100

    Updated the ReadingListSchema to include additional properties for manga such as title, manga_status, statistics, and status. This change allows for more detailed information to be stored for each manga in the reading list.

--- models/readingList.js
+++ models/readingList.js
@@ -4,6 +4,12 @@ const ReadingListSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     mangas: [{
         manga: { type: String, required: true },
+        title: { type: String },
+        manga_status: { type: String },
+        statistics: {
+            follow: { type: Number },
+            rating: { type: Number },
+        },
         status: { type: String },
         created_at: { type: Date, default: Date.now }
     }]
